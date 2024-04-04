d85a2eefe6c87574316462a2c6a61dd6dd23abd2
Spitfier720 <56697279+Spitfier720@users.noreply.github.com>
2024-04-04 17:01:54 -0400

    Fixed events[], members[] and equipment[]
    Also removed the Calendar table, it's not going to work any better than just making a table of events

--- dml.sql
+++ dml.sql
@@ -19,13 +19,11 @@ CREATE TABLE Members (
     goal VARCHAR(255),
     time_weeks INT,
     balance FLOAT
-    --events[]
 );
 
 CREATE TABLE Trainers (
     trainer_id SERIAL PRIMARY KEY,
     person_id INT REFERENCES People(person_id)
-    --events[]
 );
 
 CREATE TABLE Admin (
@@ -37,39 +35,25 @@ CREATE TABLE Events (
     event_id SERIAL PRIMARY KEY,
     trainer_name VARCHAR(255) NOT NULL,
     room INT,
-    --equipment[]
-    --members[]
     cost FLOAT,
     available INT,
     total_capacity INT
 );
 
+CREATE TABLE MemberEvents (
+    member_id INT REFERENCES Members(member_id),
+    event_id INT REFERENCES Events(event_id),
+    PRIMARY KEY (member_id, event_id)
+    day_of_week INT,
+    start_hour INT,
+    end_hour INT
+);
+
 CREATE TABLE Inventory (
     inventory_id SERIAL PRIMARY KEY,
+    event_id INT REFERENCES Events(event_id),
     equipment_name VARCHAR(255) NOT NULL,
     defective_count INT,
     total_quantity INT,
     in_use INT
 );
-
-CREATE TABLE Calendar (
-    time_week VARCHAR(11) NOT NULL,
-    monday INT,
-    tuesday INT,
-    wednesday INT,
-    thursday INT,
-    friday INT,
-    saturday INT,
-    sunday INT
-);
-
-INSERT INTO Calendar (time_week, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
-VALUES
-('9:00-10:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('10:00-11:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('11:00-12:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('13:00-15:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('15:00-17:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('17:00-19:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('19:00-21:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('21:00-22:00', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
